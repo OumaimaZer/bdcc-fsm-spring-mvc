217205b542c12cb80a4f56a1d3a1ead4742cc83d
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-06-08 19:43:59 +0100

    Security configuration

--- src/main/java/net/zerhouani/bdccfsmspringmvc/web/ProductController.java
+++ src/main/java/net/zerhouani/bdccfsmspringmvc/web/ProductController.java
@@ -1,11 +1,14 @@
 package net.zerhouani.bdccfsmspringmvc.web;
 
+import jakarta.validation.Valid;
 import net.zerhouani.bdccfsmspringmvc.entities.Product;
 import net.zerhouani.bdccfsmspringmvc.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -22,9 +25,29 @@ public class ProductController {
         return "products";
     }
 
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productList", products);
+        return "redirect:/index";
+    }
+
     @GetMapping("/delete")
     public String delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
         return "redirect:/index";
+    }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return "redirect:/newProduct";
     }
 }
